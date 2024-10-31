7669877120e83680ecdf6325c439002f97cf041a
runtyalien <omgadkar@gmail.com>
2024-10-31 12:08:27 +0530

    middleware and router added

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const videoRoutes = require('./routes/videoRoutes')
+const errorMiddleware = require('./middlewares/errorMiddleware')
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use('/api', videoRoutes);
+app.use(errorMiddleware);
 
 module.exports = app;
